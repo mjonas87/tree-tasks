356c226ad1c85c4f70536613298a6a6843837434
Matt Jonas <matt.jonas@gmail.com>
2017-05-08 01:02:06 -0700

    Integrate Vue.js

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,2 +1,94 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+window.onload = function () {
+  var tasks = new Vue({
+    el: '#tasks',
+    data: {
+      tasks: [],
+      task: {
+        name: '',
+        email: '',
+        manager: false
+      },
+      errors: {}
+    },
+    mounted: function() {
+      var that;
+      that = this;
+      $.ajax({
+        url: '/tasks.json',
+        success: function(res) {
+          that.tasks = res;
+        }
+      });
+    },
+    methods: {
+      hireTask: function () {
+        var that = this;
+        $.ajax({
+          method: 'POST',
+          data: {
+            task: that.task,
+          },
+          url: '/tasks.json',
+          success: function(res) {
+            that.errors = {}
+            that.tasks.push(res);
+          },
+          error: function(res) {
+            that.errors = res.responseJSON.errors
+          }
+        })
+      },
+    }
+  });
+
+  Vue.component('task-row', {
+    template: '#task-row',
+    props: {
+      task: Object
+    },
+    data: function () {
+      return {
+        editMode: false,
+        errors: {}
+      }
+    },
+    methods: {
+      // toggle the manager status which also updates the task in the database
+      toggleManagerStatus: function () {
+        this.task.manager = !this.task.manager
+        this.updateEmployee()
+      },
+      // ajax call for updating an task
+      updateEmployee: function () {
+        var that = this;
+        $.ajax({
+          method: 'PUT',
+          data: {
+            task: that.task,
+          },
+          url: '/tasks/' + that.task.id + '.json',
+          success: function(res) {
+            that.errors = {}
+            that.task = res
+            that.editMode = false
+          },
+          error: function(res) {
+            that.errors = res.responseJSON.errors
+          }
+        })
+      },
+      fireTask: function () {
+        var that = this;
+        $.ajax({
+          method: 'DELETE',
+          url: '/tasks/' + that.task.id + '.json',
+          success: function(res) {
+            that.$remove()
+          }
+        })
+      }
+    }
+})
+}
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,48 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+    respond_to do |format|
+      format.html
+      format.json { render :json => @tasks }
+    end
+  end
+
+  def create
+    @task = Task.new(task_params)
+    respond_to do |format|
+      format.json do
+        if @task.save
+          render :json => @task
+        else
+          render :json => { :errors => @task.errors.messages }, :status => 422
+        end
+      end
+    end
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    respond_to do |format|
+      format.json do
+        if @task.update(task_params)
+          render :json => @task
+        else
+          render :json => { :errors => @task.errors.messages }, :status => 422
+        end
+      end
+    end
+  end
+
+  def destroy
+    Task.find(params[:id]).destroy
+    respond_to do |format|
+      format.json { render :json => {}, :status => :no_content }
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :email, :manager)
+  end
 end
